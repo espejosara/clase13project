159fadfd1713097f5acd38b9f72c8d29d5f0930d
espejosara <espejo.sara93@gmail.com>
2026-07-23 19:39:45 +0200

    comentario

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
-
+import './App.css' 
 function App() {
   const [count, setCount] = useState(0)
-
+ /* .comentario */
   return (
     <>
       <section id="center">
