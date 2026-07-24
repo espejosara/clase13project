a9357fa7e3ec5b7a004447b300b853252c36c10c
espejosara <espejo.sara93@gmail.com>
2026-07-24 12:46:12 +0200

    crear layout base con rutas anidadas

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
+import Layout from '../components/Layout/Layout'
 import HomePage from '../pages/HomePage/HomePage'
 import ProductsPage from '../pages/ProductsPage/ProductsPage'
 import ProductDetailPage from '../pages/ProductDetailPage/ProductDetailPage'
@@ -7,18 +8,24 @@ import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
 export const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <HomePage />,
-	},
-	{
-		path: '/products',
-		element: <ProductsPage />,
-	},
-	{
-		path: '/products/:productId',
-		element: <ProductDetailPage />,
-	},
-	{
-		path: '*',
-		element: <NotFoundPage />,
+		element: <Layout />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />,
+			},
+			{
+				path: 'products',
+				element: <ProductsPage />,
+			},
+			{
+				path: 'products/:productId',
+				element: <ProductDetailPage />,
+			},
+			{
+				path: '*',
+				element: <NotFoundPage />,
+			},
+		],
 	},
 ])
